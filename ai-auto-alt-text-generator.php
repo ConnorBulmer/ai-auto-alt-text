82846384055b59cd6503f28640fa9135301e8347
Connor Bulmer <connor.bulmer@live.co.uk>
2025-05-07 09:04:10 +0100

    Update ai-auto-alt-text-generator.php

--- ai-auto-alt-text-generator.php
+++ ai-auto-alt-text-generator.php
@@ -3,7 +3,7 @@
  * Plugin Name: AI Auto Alt Text Generator
  * Plugin URI:  https://github.com/ConnorBulmer/ai-auto-alt-text/
  * Description: Automatically generates alt text and image titles for uploaded images using OpenAI’s GPT‑4o mini vision model, improving accessibility and SEO.
- * Version:     1.13
+ * Version:     1.14
  * Author:      Connor Bulmer
  * Author URI:  https://connorbulmer.co.uk
  * Text Domain: ai-auto-alt-text-generator
